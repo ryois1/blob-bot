e5fd9a657d8270fb191509246b6e6b0ebdba236f
Ryois <ryois@ryois.me>
2020-08-11 23:36:58 +0000

    fix restart perms

--- commands/restart.js
+++ commands/restart.js
@@ -3,9 +3,13 @@ module.exports = {
     aliases: ['reboot'],
     description: "Restarts the bot",
     async execute(message, args, client, token) {
+        if (message.member.hasPermission("ADMINISTRATOR")) {
         console.log("RESTARTED BOT")
         message.reply('Restarting the bot...')
         .then(message => client.destroy())
         .then(() => client.login(token));
+        }else {
+            message.reply(`you do not have permissions to use this command!`);
+          }
     },
   };
