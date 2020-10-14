4b60fb10661182a456e1dfdd58450a9b8a104c2d
Ryois <ryois@ryois.me>
2020-10-14 19:09:32 +0000

    add more sus messages

--- commands/sus.js
+++ commands/sus.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'Yellow sus',
 	async execute(message) {
 		const sus = message.guild.members.cache.random().user;
-		const phrases = Array(`\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was The Impostor.     .　 。　.\n　 　　。　　 1 Impostor remains.　　.　 。\`\`\``, `\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was The Impostor.     .　 。　.\n　 　　。　　 0 Impostors remain.　　.　 。\`\`\``, `${sus.username} was faking their tasks`, `${sus.username}: I was doing lights`, `byeeee ${sus.username}`, `I saw ${sus.username} vent`, `${sus.username} sus`, `${sus.username}: I'm not the impostor, u really think my fat juicy ass could fit through the vent?`);
+		const phrases = Array(`\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was not an Impostor.     .　 。　.\n　 　　。　　 2 Impostors remains.　　.　 。\`\`\``, `\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was The Impostor.     .　 。　.\n　 　　。　　 1 Impostor remains.　　.　 。\`\`\``, `\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was The Impostor.     .　 。　.\n　 　　。　　 0 Impostors remain.　　.　 。\`\`\``, `${sus.username} was faking their tasks`, `${sus.username}: I was doing lights`, `byeeee ${sus.username}`, `I saw ${sus.username} vent`, `${sus.username} sus`, `${sus.username}: I'm not the impostor, u really think my fat juicy ass could fit through the vent?`);
 		const susmessage = phrases[Math.floor(Math.random() * phrases.length)];
 		message.channel.send(susmessage);
 	},
