19b5f48574951567ca914fb96c92f879695f27a8
TurboSonic <marcianocartagena@gmail.com>
2020-10-14 15:05:54 -0400

    I hope blob is right

--- commands/minecraftip.js
+++ commands/minecraftip.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-irregular-whitespace */
 module.exports = {
 	name: 'ip',
-	description: 'minecraft server ip',
+	description: 'Send Minecraft IPs',
 	async execute(message) {
-		const phrases = Array(`\`\`\` creative-mc.blob.rocks|survival-mc.blob.rocks|mc.asspoop.com:25566`,		const susmessage = phrases[Math.floor(Math.random() * phrases.length)];
-		message.channel.send(susmessage);
+		message.channel.send('creative-mc.blob.rocks|survival-mc.blob.rocks|mc.asspoop.com:25566');
 	},
 };
