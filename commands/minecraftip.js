5b3bb82680e1b004662f7b974dc8b24bb9a84dd9
Marciano Cartagena <marcianocartagena@gmail.com>
2020-10-14 14:47:58 -0400

    if this fucking works I'm a genius

---
+++ commands/minecraftip.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-irregular-whitespace */
+module.exports = {
+	name: 'ip',
+	description: 'minecraft server ip',
+	async execute(message) {
+		const phrases = Array(`\`\`\` creative-mc.blob.rocks|survival-mc.blob.rocks|mc.asspoop.com:25566`,		const susmessage = phrases[Math.floor(Math.random() * phrases.length)];
+		message.channel.send(susmessage);
+	},
+};
