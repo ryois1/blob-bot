c6f2836245547a99cb952b080a84f173c9f07672
Ryois <ryois@ryois.me>
2020-10-16 01:36:09 +0000

    temps

--- commands/temps.js
+++ commands/temps.js
@@ -2,27 +2,27 @@ module.exports = {
 	name: 'temps',
 	description: 'Celsius to Fahrenheit and vice versa',
 	async execute(message, args) {
-        var unitFrom = args[0];
-        var from = args[1];
-        if(unitFrom == "c"){
-            var cToFahr = from * 9 / 5 + 32;
-            var output = from+'\xB0C is ' + cToFahr.toFixed(2) + ' \xB0F.';
-            message.reply(output);
-        }
-        if(unitFrom == "C"){
-            var cToFahr = from * 9 / 5 + 32;
-            var output = from+'\xB0C is ' + cToFahr.toFixed(2) + ' \xB0F.';
-            message.reply(output);
-        }
-        if(unitFrom == "f"){
-            var fToCel = (from - 32) * 5 / 9;
-            var output = from+'\xB0F is ' + fToCel.toFixed(2) + '\xB0C.';
-            message.reply(output);
-        }
-        if(unitFrom == "F"){
-            var fToCel = (from - 32) * 5 / 9;
-            var output = from+'\xB0F is ' + fToCel.toFixed(2) + '\xB0C.';
-            message.reply(output);
-        }
+		const unitFrom = args[0];
+		const from = args[1];
+		if(unitFrom == 'c') {
+			const cToFahr = from * 9 / 5 + 32;
+			const output = from + '\xB0C is ' + cToFahr.toFixed(2) + ' \xB0F.';
+			message.reply(output);
+		}
+		if(unitFrom == 'C') {
+			const cToFahr = from * 9 / 5 + 32;
+			const output = from + '\xB0C is ' + cToFahr.toFixed(2) + ' \xB0F.';
+			message.reply(output);
+		}
+		if(unitFrom == 'f') {
+			const fToCel = (from - 32) * 5 / 9;
+			const output = from + '\xB0F is ' + fToCel.toFixed(2) + '\xB0C.';
+			message.reply(output);
+		}
+		if(unitFrom == 'F') {
+			const fToCel = (from - 32) * 5 / 9;
+			const output = from + '\xB0F is ' + fToCel.toFixed(2) + '\xB0C.';
+			message.reply(output);
+		}
 	},
 };
