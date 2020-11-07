b6936a9b657dc490ffd4dc569ae69b69ff05f0ee
Ryois <ryois@ryois.me>
2020-11-07 03:04:05 +0000

    Sus Messages

--- commands/sus.js
+++ commands/sus.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-irregular-whitespace */
+const { messages } = require('../content/sus_messages.json');
+
 module.exports = {
 	name: 'sus',
 	cooldown: 0.5,
@@ -6,8 +7,10 @@ module.exports = {
 	description: 'Yellow sus',
 	async execute(message) {
 		const sus = message.guild.members.cache.random().user;
-		const phrases = Array(`\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was not an Impostor.     .　 。　.\n　 　　。　　 2 Impostors remains.　　.　 。\`\`\``, `\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was The Impostor.     .　 。　.\n　 　　。　　 1 Impostor remains.　　.　 。\`\`\``, `\`\`\` 　.　　      。　        ඞ   。　    .    •\n   •         ${sus.username} was The Impostor.     .　 。　.\n　 　　。　　 0 Impostors remain.　　.　 。\`\`\``, `${sus.username} was faking their tasks`, `${sus.username}: I was doing lights`, `byeeee ${sus.username}`, `I saw ${sus.username} vent`, `${sus.username} sus`, `${sus.username}: I'm not the impostor, u really think my fat juicy ass could fit through the vent?`);
-		const susmessage = phrases[Math.floor(Math.random() * phrases.length)];
+		const susmessage = messages[Math.floor(Math.random() * messages.length)]
+			.slice(0)
+			.replace('$username', sus.username)
+        ;
 		message.channel.send(susmessage);
 	},
 };
