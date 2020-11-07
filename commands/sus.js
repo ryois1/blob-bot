0747f5e1d4bf90aab0f7505809e4aa7639db9457
Ryois <ryois@ryois.me>
2020-11-07 20:16:09 +0000

    Even more sus shit

--- commands/sus.js
+++ commands/sus.js
@@ -7,14 +7,13 @@ module.exports = {
 	description: 'Yellow sus',
 	async execute(message) {
 		const { messages, places, colors } = JSON.parse(fs.readFileSync('./content/sus_messages.json'));
-		const user_sus = message.guild.members.cache.random().user;
-		const user_killed = message.guild.members.cache.random().user;
+		const user_sus = message.guild.members.cache.random();
+		const user_killed = message.guild.members.cache.random();
 		const place = places[Math.floor(Math.random() * places.length)];
 		const color = colors[Math.floor(Math.random() * colors.length)];
 		const susmessage = messages[Math.floor(Math.random() * messages.length)]
-			.slice(0)
-			.replace('$user_sus', `**${user_sus.username}**`)
-			.replace('$user_killed', `**${user_killed.username}**`)
+			.replace('$user_sus', `**${user_sus.displayName}**`)
+			.replace('$user_killed', `**${user_killed.displayName}**`)
 			.replace('$place', `**${place}**`)
 			.replace('$color', `**${color}**`)
         ;
