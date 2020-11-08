ff7cf86002e854d4ae5e4aebf4dd9c2880d27f95
Ryois <ryois@ryois.me>
2020-11-07 23:32:12 -0500

    Revert "Merge branch 'master' into 'master'"
    This reverts merge request !4

--- commands/sus.js
+++ commands/sus.js
@@ -6,40 +6,18 @@ module.exports = {
 	usage: '[Command only]',
 	description: 'Yellow sus',
 	async execute(message) {
-		const { channel } = message;
 		const { messages, places, colors } = JSON.parse(fs.readFileSync('./content/sus_messages.json'));
-
-		const user_accused = message.guild.members.cache.random().user;
 		const user_sus = message.guild.members.cache.random().user;
-
 		const user_killed = message.guild.members.cache.random().user;
 		const place = places[Math.floor(Math.random() * places.length)];
 		const color = colors[Math.floor(Math.random() * colors.length)];
-
 		const susmessage = messages[Math.floor(Math.random() * messages.length)]
 			.slice(0)
 			.replace('$user_sus', `**${user_sus.username}**`)
 			.replace('$user_killed', `**${user_killed.username}**`)
 			.replace('$place', `**${place}**`)
 			.replace('$color', `**${color}**`)
-		;
-
-
-		const hooks_len = (await channel.fetchWebhooks()).size;
-
-		if (hooks_len == 0) {
-			await channel.createWebhook('sus', {
-				avatar: 'https://cdn.discordapp.com/attachments/765292850219843645/774848748861390888/5fa5425f96cc0.png',
-				reason: 'sus'
-			});
-		}
-	
-		const hooks = await channel.fetchWebhooks();
-		const webhook = hooks.first();
-	
-		webhook.send(susmessage, {
-			username: `${user_accused.member.displayName}`,
-			avatarURL: message.author.displayAvatarURL()
-		});
+        ;
+		message.channel.send(susmessage);
 	},
 };
