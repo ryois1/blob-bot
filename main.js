3e010af3c239dcc468f46c1086b7bb3b3ec03053
Ryois <ryois@ryois.me>
2021-02-12 02:53:48 +0000

    Updated stuff

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ const token = config.token;
 const gitlab = process.env.GITLAB;
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+
 if(gitlab) {
 	console.log('Success!');
 	process.exit();
 }
+
 const commandFiles = fs
 	.readdirSync('./commands')
 	.filter((file) => file.endsWith('.js'));
@@ -19,22 +21,22 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
+
 client.on('message', (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
-	const command =
-	client.commands.get(commandName) ||
-	client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName),
-	);
+	const command = client.commands.get(commandName) || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 
 	if (!command) return;
+
 	if (!command.enabled) {
 		return message.reply('that command is disabled.') .then(msg => {
 			msg.delete({ timeout: 2500 });
 			message.delete({ timeout: 2500 });
 		});
 	}
+
 	if(command.allowedGuilds) {
 		if (!command.allowedGuilds.includes(`${message.guild.id}`)) {
 			return message.reply('that command is disabled in this guild.') .then(msg => {
@@ -43,6 +45,7 @@ client.on('message', (message) => {
 			});
 		}
 	}
+
 	if(command.ownerOnly) {
 		if (message.author.id != '236237361703288842') {
 			return message.reply('that command is only available to the owner.') .then(msg => {
@@ -51,18 +54,23 @@ client.on('message', (message) => {
 			});
 		}
 	}
+
 	if (command.args && !args.length) {
 		return message.reply(`that command requires some arguments... ${prefix}${command.name} ${command.usage}`);
 	}
+
 	try {
 		command.execute(message, args, client, token, config, logger);
 	}
+
 	catch (error) {
 		logger.error(client, error);
 		message.reply('there was an error trying to execute that command!');
 	}
+
 });
 client.login(token);
+
 client.once('ready', () => {
 	logger.log(client, 'Started blob bot!');
 	logger.log(client, `blob bot is in ${client.guilds.cache.size} servers`);
