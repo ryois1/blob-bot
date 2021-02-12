cbdc8794c0c4437df696a5ad04dfbc29cebd3eb1
Ryois <ryois@ryois.me>
2021-02-12 20:53:43 +0000

    Added timestamps to logs

--- modules/logger.js
+++ modules/logger.js
@@ -1,19 +1,24 @@
 function log(client, message) {
 	const chalk = require('chalk');
 	const config = require('../private/config.json');
-	console.log(chalk.green(`[BLOB-BOT LOG] ${message}`));
-	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT LOG] ${message}`);
+	const moment = require('moment');
+	const current_time = moment().format('M/d/YYYY, h:mm:ss A');
+	console.log(chalk.green(`[BLOB-BOT LOG - ${current_time}] ${message}`));
+	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT LOG - ${current_time}] ${message}`);
 }
 function error(client, message) {
 	const chalk = require('chalk');
+	const moment = require('moment');
+	const current_time = moment().format('M/d/YYYY, h:mm:ss A');
 	const config = require('../private/config.json');
-	console.log(chalk.red(`[BLOB-BOT ERROR] ${message}`));
+	console.log(chalk.red(`[BLOB-BOT ERROR - ${current_time}] ${message}`));
 	const Sentry = require('@sentry/node');
+
 	Sentry.init({
 		dsn: config.SENTRY_DSN,
 		tracesSampleRate: 1.0,
 	});
 	Sentry.captureException(message);
-	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT ERROR] ${message}`);
+	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT ERROR - ${current_time}] ${message}`);
 }
 module.exports = { log, error };
