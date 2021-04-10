2456965003ca5d1654e461a16576e810d4c79a2c
Ryois <ryois@ryois.me>
2021-04-10 01:11:23 +0000

    Fix sentry errors

--- modules/logger.js
+++ modules/logger.js
@@ -17,15 +17,13 @@ function error(client, message) {
 	const current_time = moment().format('M/d/YYYY, h:mm:ss A');
 	const config = require('../private/config.json');
 	console.log(chalk.red(`[BLOB-BOT ERROR - ${current_time}] ${message}`));
+	console.log(message);
 	const Sentry = require('@sentry/node');
 	Sentry.init({
 		dsn: config.SENTRY_DSN,
 		tracesSampleRate: 1.0,
 	});
 	Sentry.captureException(message);
-	axios.post('https://haste.ryois.me/documents', { message })
-		.then(function(response) {
-			client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT ERROR - ${current_time}] https://haste.ryois.me/${response.data.key}`);
-		});
+	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT ERROR - ${current_time}] Please check Sentry`);
 }
 module.exports = { log, error };
