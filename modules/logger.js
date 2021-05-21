1f556e5c09367439851fc5eefd74c2cbde4e8482
Ryois <ryois@ryois.me>
2021-05-21 21:12:06 +0000

    Sentry remove

--- modules/logger.js
+++ modules/logger.js
@@ -1,29 +1,13 @@
-const axios = require('axios');
-
+const chalk = require('chalk');
+const moment = require('moment')
 function log(client, message) {
-	const chalk = require('chalk');
-	const config = require('../private/config.json');
-	const moment = require('moment');
 	const current_time = moment().format('M/d/YYYY, h:mm:ss A');
 	console.log(chalk.green(`[BLOB-BOT LOG - ${current_time}] ${message}`));
-	axios.post('https://haste.ryois.me/documents', { message })
-		.then(function(response) {
-			client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT LOG - ${current_time}] https://haste.ryois.me/${response.data.key}`);
-		});
+	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT LOG - ${current_time}] ${message}`);
 }
 function error(client, message) {
-	const chalk = require('chalk');
-	const moment = require('moment');
 	const current_time = moment().format('M/d/YYYY, h:mm:ss A');
-	const config = require('../private/config.json');
 	console.log(chalk.red(`[BLOB-BOT ERROR - ${current_time}] ${message}`));
-	console.log(message);
-	const Sentry = require('@sentry/node');
-	Sentry.init({
-		dsn: config.SENTRY_DSN,
-		tracesSampleRate: 1.0,
-	});
-	Sentry.captureException(message);
-	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT ERROR - ${current_time}] Please check Sentry`);
+	client.channels.cache.get(config.blob_bot_log_channel).send(`[BLOB-BOT ERROR - ${current_time}] ${message}`);
 }
 module.exports = { log, error };
