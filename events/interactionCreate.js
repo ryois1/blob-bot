331acb498e7bad67a18b49a0995c3a7f032d252a
Ryois <ryois@ryois.me>
2023-01-01 22:22:55 +0000

    Add command enabled function

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,73 +1,50 @@
 const { Events } = require('discord.js');
 
+
 module.exports = {
 	name: Events.InteractionCreate,
+	async isCommandEnabled(interaction, command) {
+		return new Promise((resolve) => {
+			if (!command.enabled) resolve([false, 'command is gobally disabled']);
+			if (command.ownerOnly) {
+				if (interaction.author.id != '236237361703288842') resolve([false, 'command is only available to the bot owner.']);
+			}
+			if (interaction.guildId == null) resolve([true]);
+			interaction.client.db.query('SELECT status FROM commands_guilds WHERE command_name = ? AND guild_id = ?', [interaction.commandName, interaction.guild.id], async function(error, result) {
+				if (error) console.log(error);
+				if (result.length > 0) {
+					if (result[0].status) resolve([true]);
+					else resolve([false, 'command is disabled for this server']);
+				}
+				else { resolve([false, 'command is disabled for this server']); }
+			});
+		});
+	},
 	async execute(interaction) {
 		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) {
+			await interaction.reply({ content: 'No command found.', ephemeral: true });
 			console.error(`No command matching ${interaction.commandName} was found.`);
 			return;
 		}
 
-		if (!command.enabled) {
-			await interaction.reply({ content: 'This command is disabled.', ephemeral: true });
+		const isEnabled = await this.isCommandEnabled(interaction, command);
+		if (isEnabled[0]) {
+			try {
+				await command.execute(interaction);
+			}
+			catch (errorCmd) {
+				await interaction.reply({ content: 'There was an error executing the command.', ephemeral: true });
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(errorCmd);
+			}
+		}
+		else {
+			await interaction.reply({ content: `This ${isEnabled[1]}.`, ephemeral: true });
 			return;
 		}
-		if (command.ownerOnly) {
-			if (interaction.author.id != '236237361703288842') {
-				await interaction.reply({ content: 'This command is only available to the owner', ephemeral: true });
-				return;
-			}
-		}
-
-		interaction.client.db.query('SELECT guild_id FROM commands_disabled_guilds WHERE command_name = ? AND guild_id = ?', [interaction.commandName, interaction.guild.id], async function(disabledError, disabledResult) {
-			if (disabledError) console.log (disabledError);
-			if (disabledResult.length > 0) {
-				console.log('Command disabled in DB disabled_guilds');
-				if (disabledResult) {
-					await interaction.reply({ content: 'This command is disabled in this guild', ephemeral: true });
-					return;
-				}
-			}
-			else {
-				interaction.client.db.query('SELECT guild_id FROM commands_enabled_guilds WHERE command_name = ? AND guild_id = ?', [interaction.commandName, interaction.guild.id], async function(enabledError, enabledResult) {
-					if (enabledError) console.log (enabledError);
-					if (!enabledResult.length) {
-						console.log('Command disabled in DB not in enabled_guilds');
-						await interaction.reply({ content: 'This command is disabled in this guild', ephemeral: true });
-						return;
-					}
-					else {
-						try {
-							await command.execute(interaction);
-						}
-						catch (errorCmd) {
-							console.error(`Error executing ${interaction.commandName}`);
-							console.error(errorCmd);
-						}
-					}
-				});
-			}
-		});
-
-		/* if (command.disabledGuilds) {
-			if (command.disabledGuilds.includes(`${interaction.guild.id}`)) {
-				await interaction.reply({ content: 'This command is disabled in this guild', ephemeral: true });
-				return;
-			}
-		} */
-
-
-		/* if (command.allowedGuilds) {
-			if (!command.allowedGuilds.includes(`${interaction.guild.id}`)) {
-				await interaction.reply({ content: 'This command is disabled in this guild', ephemeral: true });
-				return;
-			}
-		} */
-
-
 	},
 };
